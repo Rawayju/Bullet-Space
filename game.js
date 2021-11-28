7ae19916d6faffc2173bc89771cc54568bb5c191
Defales <rawatjuart@gmail.com>
2021-11-28 20:30:02 -0300

    Changed and added scenes

--- game.js
+++ game.js
@@ -1,15 +1,15 @@
 var gameSettings = {
-    shipSpeed: 1.50,
-    shiftControl: 0.55,
+    shipSpeed: 0.375,
+    shiftControl: 0.5,
     fireRate: 3,
-    fireSpeed: 600
+    fireSpeed: 200
 }
 
 var config = {
     type: Phaser.AUTO,
-    width: 246 * 4,
-    height: 156 *4,
-    scene: [Scene1, Scene2],
+    width: 246,
+    height: 156,
+    scene: [SCNbootGame, SCNstartTestLevel, SCNplayTestLevel],
     pixelArt: true,
     render: {
         antialias: true,
