dbc04ea33c4df8a1c5a787057b27fbeac86e3da7
Defales <rawatjuart@gmail.com>
2021-11-30 18:59:54 -0300

    Added properties

--- game.js
+++ game.js
@@ -1,8 +1,16 @@
 var gameSettings = {
-    shipSpeed: 0.375,
+    shipSpeed: 0.475,
     shiftControl: 0.5,
-    fireRate: 3,
-    fireSpeed: 200
+    fireRate: 1.5,
+    fireSpeed: 500,
+    asteroidSpawnRate: 300,
+    levelAsteroids: {
+        big: 20,
+        medium: 20,
+        small: 20,
+    },
+    levelDifficulty: 100,
+    handicap: 100
 }
 
 var config = {
